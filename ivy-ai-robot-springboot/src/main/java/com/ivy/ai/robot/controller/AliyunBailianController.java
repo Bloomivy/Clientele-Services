c9426cbbc62642f9ceaea08e4982ec94e979410e
WuQFu <2083834998@qq.com>
2026-01-10 20:07:50 +0800

    feat: 首次提交，整合前后端代码

---
+++ ivy-ai-robot-springboot/src/main/java/com/ivy/ai/robot/controller/AliyunBailianController.java
@@ -0,0 +1,60 @@
+package com.ivy.ai.robot.controller;
+
+import com.ivy.ai.robot.model.AIResponse;
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Objects;
+/**
+ * @Author: Ivy
+ * @Date: 2025/6/2 21:58
+ * @Version 1.00
+ * @Description 对接阿里云百炼
+ */
+@RestController
+@RequestMapping("/v6/ai")
+public class AliyunBailianController {
+
+    @Resource
+    private OpenAiChatModel chatModel;
+
+    /**
+     * 普通对话
+     * @param message
+     * @return
+     */
+    @GetMapping("/generate")
+    public String generate(@RequestParam(value = "message", defaultValue = "你是谁？") String message) {
+        // 一次性返回结果
+        return chatModel.call(message);
+    }
+
+    /**
+     * 流式对话
+     * @param message
+     * @return
+     */
+    @GetMapping(value = "/generateStream",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<AIResponse> generateStream(@RequestParam(value = "message", defaultValue = "你是谁？") String message) {
+        // 构建提示词
+        Prompt prompt = new Prompt(new UserMessage(message));
+
+        // 流式输出
+        return chatModel.stream(prompt)
+                .mapNotNull(chatResponse -> {
+                    Generation generation = chatResponse.getResult();
+                    String text = generation.getOutput().getText();
+                    return AIResponse.builder().v(text).build();
+                });
+    }
+
+}
