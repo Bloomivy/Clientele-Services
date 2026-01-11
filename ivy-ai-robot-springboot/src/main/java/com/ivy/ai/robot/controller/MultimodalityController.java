ecf09a4cb2a6003d288c547a66251aa21f6328ba
WuQFu <2083834998@qq.com>
2026-01-11 13:00:16 +0800

    feat：使用阿里云模型，实现多模态：文生图、文生视频、文生音频

---
+++ ivy-ai-robot-springboot/src/main/java/com/ivy/ai/robot/controller/MultimodalityController.java
@@ -0,0 +1,63 @@
+package com.ivy.ai.robot.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.content.Media;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: Ivy
+ * @Date: 2026/1/11 12:18
+ * @Version 1.00
+ * @Description：多模态
+ */
+@RequestMapping("/v9/ai")
+@RestController
+public class MultimodalityController {
+
+    @Resource
+    private OpenAiChatModel chatModel;
+
+    @GetMapping(value = "generateStream", produces = "text/html;charset=utf-8")
+    public Flux<String> generateStream(@RequestParam(value = "message") String message) {
+
+        // 1.创建媒体资源
+        Media image = new Media(
+                MimeTypeUtils.IMAGE_JPEG,
+                new ClassPathResource("/images/tomato.jpg")
+        );
+
+        // 2. 附加选项（可选），如温度值等等
+        Map<String, Object> metadata = new HashMap<>();
+        metadata.put("temperature", 0.7);
+
+        // 3. 构建多模态消息
+        UserMessage userMessage = UserMessage.builder()
+                .text(message)
+                .media(image)
+                .metadata(metadata)
+                .build();
+
+        // 4. 构建提示词
+        Prompt prompt = new Prompt(List.of(userMessage));
+        // 5. 流式调用
+        return chatModel.stream(prompt)
+                .mapNotNull(chatResponse -> {
+                    Generation generation = chatResponse.getResult();
+                    return generation.getOutput().getText();
+                });
+    }
+}
