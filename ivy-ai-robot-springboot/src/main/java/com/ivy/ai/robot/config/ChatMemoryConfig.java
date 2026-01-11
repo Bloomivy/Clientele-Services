5c3f7a5ea5652522eaf31f3302c88411c61133f4
WuQFu <2083834998@qq.com>
2026-01-11 12:13:05 +0800

    feat：整合Cassandra实现聊天消息持久化

--- ivy-ai-robot-springboot/src/main/java/com/ivy/ai/robot/config/ChatMemoryConfig.java
+++ ivy-ai-robot-springboot/src/main/java/com/ivy/ai/robot/config/ChatMemoryConfig.java
@@ -4,6 +4,7 @@ import jakarta.annotation.Resource;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.cassandra.CassandraChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,9 +21,12 @@ public class ChatMemoryConfig {
     * @Description:  记忆存储
     * @Author: Ivy
     */
+//    @Resource
+//    private ChatMemoryRepository chatMemoryRepository;
     @Resource
-    private ChatMemoryRepository chatMemoryRepository;
-    
+    private CassandraChatMemoryRepository chatMemoryRepository;
+
+
     /**
     * @Description: 初始化一个 ChatMemory 实例，并注入到 Spring 容器中
     * @Author: Ivy
